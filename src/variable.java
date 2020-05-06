9af3d91a21aa3c45cea813f7ba86be1c0a09ea61
javeria <javeria.khaliq3042@gmail.com>
2020-05-06 15:41:50 -0400

    Java files

---
+++ src/variable.java
@@ -0,0 +1,26 @@
+
+public class variable {
+
+	public static void main(String[] args) {
+		// 8 type of primitive data type in Java.
+		int a = 10;
+		double d = 20.0;
+		float f = 209f;
+		char c = 'a';
+		long l = 46657;
+		byte b = 48;
+		boolean s = true;
+		short s1 = 5656;
+
+		System.out.println(a);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(c);
+		System.out.println(l);
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(s1);
+
+	}
+
+}
