3122339bb6cce306cb2aaff4a0b852e5b0e15b3c
javeria <javeria.khaliq3042@gmail.com>
2020-05-06 16:06:07 -0400

    string

--- src/string.java
+++ src/string.java
@@ -2,6 +2,10 @@
 public class string {
 
 	public static void main(String[] args) {
+		String text = "AFpple";
+		String text1 = " ";
+		String Text3 = "Greating";
+		System.out.println(text + text1 + Text3);
 
 	}
 
