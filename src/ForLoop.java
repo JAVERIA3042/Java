5c0f2cb563853500868302e9ab2738e0533459ee
javeria <javeria.khaliq3042@gmail.com>
2020-05-06 16:35:28 -0400

    for loop

---
+++ src/ForLoop.java
@@ -0,0 +1,9 @@
+
+public class ForLoop {
+	public static void main(String[] args) {
+		for(int i=0; i<5; i++) {
+			System.out.println("Hello");
+			System.out.printf("Hello everyone: %d\n" ,i);
+		}
+	}
+}
